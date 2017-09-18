ac7b00edeb19113df93d9527d99f05618c1895ba
Annika Noren <amenoren@gmail.com>
2017-09-18 22:00:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string){
     if (string.toLowerCase() === string){
       return("I can't hear you!")
     } else if (string.toUpperCase() === string){
-      return("YES, INDEED!")
+      return("YES INDEED!")
     } else {
       return("I love you, too")
     }
